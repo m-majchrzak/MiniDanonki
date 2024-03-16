4bd8fe2f516ddbc94c778210650c3e7b00ee6461
Martyna Majchrzak <martyna.majchrzak19@gmail.com>
2024-03-16 13:15:05 +0100

    data

--- modelstealing/load_dataset.py
+++ modelstealing/load_dataset.py
@@ -3,6 +3,7 @@ from taskdataset import TaskDataset
 
 
 if __name__ == "__main__":
-    dataset = torch.load("modelstealing/data/ExampleModelStealingPub.pt")
+    dataset = torch.load("modelstealing/data/ModelStealingPub.pt")
 
     print(dataset.ids, dataset.imgs, dataset.labels)
+
