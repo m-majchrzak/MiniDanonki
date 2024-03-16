f96e42d3b1aa0a0a0c385c3cb57459e10e1b5aac
Martyna Majchrzak <martyna.majchrzak19@gmail.com>
2024-03-16 17:52:07 +0100

    model stealing scripts update

--- modelstealing/model_stealing.py
+++ modelstealing/model_stealing.py
@@ -1,18 +1,21 @@
 import requests
 
+        
 def model_stealing(path_to_png_file: str):
-    SERVER_URL = "[paste server url here]"
-    ENDPOINT = "/modelstealing"
-    URL = SERVER_URL + ENDPOINT
-
+    SERVER_URL = "http://34.71.138.79:9090"
+    endpoint = "/modelstealing"
     TEAM_TOKEN = "X55E27lOG6LS3QRm"
-
-    with open(path_to_png_file, "rb") as img_file:
-        response = requests.get(
-            URL, files={"file": img_file}, headers={"token": TEAM_TOKEN}
-        )
-
+    url = SERVER_URL + endpoint
+    with open(path_to_png_file, "rb") as f:
+        response = requests.get(url, files={"file": f}, headers={"token": TEAM_TOKEN})
         if response.status_code == 200:
-            return response.content["representation"]
+            representation = response.json()["representation"]
+            print("Request ok")
+            print(representation)
         else:
-            raise Exception(f"Request failed. Status code: {response.status_code}, content: {response.content}")
+            raise Exception(
+                f"Model stealing failed. Code: {response.status_code}, content: {response.json()}"
+            )
+    print("Hello world!")
+if __name__ == "__main__":
+    model_stealing("modelstealing/data/labels_ids_png/6661858_163933.png")
--- modelstealing/model_stealing_reset.py
+++ modelstealing/model_stealing_reset.py
@@ -1,17 +1,15 @@
 import requests
 
 def model_stealing_reset():
-    SERVER_URL = "[paste server url here]"
-    ENDPOINT = "/modelstealing/reset"
-    URL = SERVER_URL + ENDPOINT
-
+    SERVER_URL = "http://34.71.138.79:9090"
     TEAM_TOKEN = "X55E27lOG6LS3QRm"
-
-    response = requests.post(
-        URL, headers={"token": TEAM_TOKEN}
-    )
-
+    endpoint = f"/modelstealing/reset"
+    url = SERVER_URL + endpoint
+    response = requests.post(url, headers={"token": TEAM_TOKEN})
     if response.status_code == 200:
-        print("Endpoint rested successfully")
+        print("Request ok")
+        print(response.json())
     else:
-        raise Exception(f"Request failed. Status code: {response.status_code}, content: {response.content}")
+        raise Exception(
+            f"Model stealing reset failed. Code: {response.status_code}, content: {response.json()}"
+        )
