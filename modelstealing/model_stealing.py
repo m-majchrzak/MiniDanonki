64e13fa4915cfd8ff537b0111f51c69a729ebbcc
Martyna Majchrzak <martyna.majchrzak19@gmail.com>
2024-03-17 08:29:40 +0100

    model stealing representations

--- modelstealing/model_stealing.py
+++ modelstealing/model_stealing.py
@@ -1,5 +1,6 @@
 import requests
-
+from datetime import datetime
+import numpy as np
         
 def model_stealing(path_to_png_file: str):
     SERVER_URL = "http://34.71.138.79:9090"
@@ -11,11 +12,15 @@ def model_stealing(path_to_png_file: str):
         if response.status_code == 200:
             representation = response.json()["representation"]
             print("Request ok")
-            print(representation)
+            return representation
         else:
             raise Exception(
                 f"Model stealing failed. Code: {response.status_code}, content: {response.json()}"
             )
-    print("Hello world!")
 if __name__ == "__main__":
-    model_stealing("modelstealing/data/labels_ids_png/6661858_163933.png")
+
+    timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+    files_to_fetch = ["6661858_163933"]
+    for file in files_to_fetch:
+        representation = model_stealing("modelstealing/data/labels_ids_png/{}.png".format(file))
+        np.savez("modelstealing/representations/{}_{}.npz".format(file, timestamp), representation=representation)
